5d7557263f729a280806595bb125748bec70e448
allstar4345 <gyf803@my.utsa.edu>
2013-10-31 13:15:06 -0500

    first changes to BPS, mostly language editing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Return a title on a per-page basis.
   def title
-    base_title = "Family Connect"
+    base_title = "The Blueprint Sports"
     if @title.nil?
       base_title
     else
@@ -11,6 +11,6 @@ module ApplicationHelper
   end
   
   def logo
-    image_tag("tree.gif", :alt => "Family Connect", :class => "round")
+    image_tag("tree.gif", :alt => "The Blueprint Sports", :class => "round")
   end
 end
